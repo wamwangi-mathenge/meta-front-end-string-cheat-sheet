0ffe8ce8d6da1240b7dc544399bd33ebff05eabf
Brian <wamwangimathenge@gmail.com>
2023-07-26 12:28:14 +0300

    Read each character at specific index in a string
    This commit uses the charAt() method to read each individual character
    at a specific index in a string

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ var place = "World";
 
 console.log(greet.length);
 
+console.log(greet.charAt(0));
+console.log(greet.charAt(2));
+console.log(greet.charAt(4));
+
 var greetingPhrase = "Wo".concat("rl").concat("d");
 console.log(greetingPhrase)
